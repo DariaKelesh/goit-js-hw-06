75a383c80d4534df812051afd8edc6a1cdfb2105
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-09 12:26:24 +0300

    task 3

--- src/src/js/task-03.js
+++ src/src/js/task-03.js
@@ -12,3 +12,29 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// const imgAttributes1 = images[0];
+// console.log(imgAttributes1);
+
+// const createImgFromArr = (array) =>
+//     array.map((element) => `<img> ${imgAttributes1} </img>`);
+
+// console.log(createImgFromArr(images));
+
+const arrUrl = [];
+const arrAlt = [];
+
+for (const image of images) {
+    arrUrl.push(image.url);
+    arrAlt.push(image.alt)
+}
+// console.log(arrUrl);
+// console.log(arrAlt);
+
+const imgCollection = `<li><img src = "${arrUrl[0]}" alt = "${arrAlt[0]}"></img></li> <li><img src = "${arrUrl[1]}" alt = "${arrAlt[1]}"></img></li> <li><img src = "${arrUrl[2]}" alt = "${arrAlt[2]}"></img></li>` ;
+// console.log(imgCollection);
+
+const body = document.querySelector("body");
+
+body.insertAdjacentHTML("beforeend", imgCollection);
+
