5267e2564130924b0dc0c321b78b626ff8e93612
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-09 15:56:17 +0300

    Update task-04.js

---
+++ src/src/js/task-04.js
@@ -0,0 +1,31 @@
+let counterValue = document.querySelector("span#value");
+counterValue = 0;
+
+const counter = document.querySelector("div#counter")
+
+const btnDecrement = document.querySelector("button[data-action=decrement]");
+
+const btnIncrement = document.querySelector("button[data-action=increment]");
+
+const onBtnDecrementClick = () => {
+    console.log("Button -1 was clicked");
+    // console.log(counterValue -= 1);
+    // КАК counterValue вывести в HTML???
+    
+    btnDecrement.insertAdjacentHTML("afterend", counterValue -= 1)
+    
+};
+
+btnDecrement.addEventListener("click", onBtnDecrementClick);
+
+
+const onBtnIncrementClick = () => {
+    console.log("Button +1 was clicked");
+    // console.log(counterValue += 1);
+    // КАК counterValue вывести в HTML???
+    btnDecrement.insertAdjacentHTML("afterend", counterValue += 1)
+};
+
+btnIncrement.addEventListener("click", onBtnIncrementClick);
+
+
