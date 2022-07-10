ba84211e5c4476dfeb8a3e781812b5318e827371
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-10 21:29:09 +0300

    Update task-08.js

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,25 @@
 
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+         } = event.currentTarget
+
+    if (email.value === "" || password.value === "") {
+        alert("Все поля должны быть заполнены");
+    }
+
+    console.log(`Email: ${email.value}, password: ${password.value}`);
+
+    event.currentTarget.reset();
+}
+
+
+   
+
