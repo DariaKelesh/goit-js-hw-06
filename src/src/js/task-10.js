57a46b37ff99f60afa42fb77d13277cd6c9fe591
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-13 11:30:32 +0300

    task 5 changed a little

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -19,6 +19,59 @@
 // }
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
 
+const input = document.querySelector("input");
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", onCreateBtnClick);
+destroyBtn.addEventListener("click", onDestroyBtnClick);
+
+console.log(input.value);
+
+// function onCreateBtnClick() {
+//  event.preventDefault();
+
+// input.addEventListener("input", (event) => {
+//   amount.textContent = event.currentTarget.value;
+// });
+//     if (amount === "") {
+//         return
+//     }
+// }
+
+// function createBoxes(amount) {
+   
+    // РАБОТАЕТ, НО СДЕЛАТЬ ЦИКЛ!
+//     const box1 = document.createElement("div");
+//     box1.style.width = "30px";
+//     box1.style.height = "30px";
+//     box1.style.backgroundColor = "tomato";
+
+//     boxes.append(box1);
+//     console.log(boxes);
+    
+// }
+
+
+function onCreateBtnClick(number) {
+    input.addEventListener("input", (event) =>
+    {
+        if (event.currentTarget.value === "") {
+            return
+        }
+        number = event.currentTarget.value
+    }
+        
+    )
+    console.log(number);
+}
+
+function onDestroyBtnClick() {
+
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
