5e831e08288c339e728b4eb723d6066a2e6a2bdc
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-13 12:43:00 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,24 +3,17 @@
 const input = document.querySelector('#validation-input');
 const dataLength = document.querySelector('[data-length]')
 
-// console.dir(dataLength.dataset.length);
-// console.log(input.value.length);
-// console.dir(input.textContent.length);
-// console.log(input.textContent.length === dataLength.dataset.length);
-
-// console.log(Number(dataLength.dataset.length));
 
 input.addEventListener("blur", (event) => {
-    event.preventDefault();
-   
-    // console.log(dataLength.dataset);
-    
+
     if (input.value.length === Number(dataLength.dataset.length)) {
 
         input.classList.add('valid');
-        console.log(input.classList);
+        input.classList.remove('invalid')
+      
     } else {
         input.classList.add('invalid')
-        console.log(input.classList);
+        input.classList.remove('valid')
+   
     }
 })
